dd063cf6565e452de17f123034873be606ccec44
Kedriik <kedro123@gmail.com>
2017-09-02 23:27:40 +0200

    continuation

--- Deep Learning A-Z/Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_all_functions.py
+++ Deep Learning A-Z/Get the Machine Learning basics/Data Preprocessing Template/data_preprocessing_all_functions.py
@@ -9,6 +9,22 @@ dataset = pd.read_csv('Data.csv')
 X=dataset.iloc[:,:-1].values      #take all columns except last one
 y=dataset.iloc[:, -1].values      #take last column
 
+#taking care of mising data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)       #ctrl+I to inspect
+imputer=imputer.fit(X[:, 1:3])
+X[:, 1:3]=imputer.transform(X[:, 1:3])
+
+#Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X=LabelEncoder()
+X[:,0]=labelencoder_X.fit_transform(X[:, 0])
+onehotencoder=OneHotEncoder(categorical_features=[0])
+X=onehotencoder.fit_transform(X).toarray()
+
+labelencoder_y=LabelEncoder()
+y=labelencoder_y.fit_transform(y)
+
 #Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
