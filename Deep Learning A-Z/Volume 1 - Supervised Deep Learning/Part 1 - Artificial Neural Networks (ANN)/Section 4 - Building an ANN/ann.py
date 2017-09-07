2888e4b540ac8c610d634020315c2cf45738dd2b
Kedriik <kedro123@gmail.com>
2017-09-08 01:24:33 +0200

    tutorial continuation

--- Deep Learning A-Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
+++ Deep Learning A-Z/Volume 1 - Supervised Deep Learning/Part 1 - Artificial Neural Networks (ANN)/Section 4 - Building an ANN/ann.py
@@ -54,10 +54,12 @@ from keras.layers import Dense
 #Initialising the ANN
 classifier = Sequential()
 
-#Adding the input layer and the first hidden layer
+#Adding the input layer and the first hidden layer with dropout
 classifier.add(Dense(output_dim=6,init='uniform', activation='relu', input_dim=11))
+classifier.add(Dropout(p=0.1))
 # adding the second hidden layer
 classifier.add(Dense(output_dim=6,init='uniform', activation='relu'))
+classifier.add(Dropout(p=0.1))
 # adding the output layer
 classifier.add(Dense(output_dim=1,init='uniform', activation='sigmoid')) #depended soft max function if more than two cathegorical data
 #Compiling the ANN
@@ -104,6 +106,7 @@ from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout #randomly disable neurons
 def build_classifier():
     classifier = Sequential()
     classifier.add(Dense(output_dim=6,init='uniform', activation='relu', input_dim=11))
@@ -116,5 +119,30 @@ accuracies = cross_val_score(estimator=classifier,X=X_train, y=y_train, cv=10, n
 mean=accuracies.mean()
 variance=accuracies.std()
 #improving the ANN
-# Dropout regularization to reduce overfitting if needed
+#Dropout regularization to reduce overfitting if needed
+
+#tuning the ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout #randomly disable neurons
+def build_classifier(optimizer):
+    classifier = Sequential()
+    classifier.add(Dense(output_dim=6,init='uniform', activation='relu', input_dim=11))
+    classifier.add(Dense(output_dim=6,init='uniform', activation='relu'))
+    classifier.add(Dense(output_dim=1,init='uniform', activation='sigmoid')) 
+    classifier.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'] )
+    return classifier
+classifier = KerasClassifier(build_fn=build_classifier)
+parameters = {'batch_size' : [25, 32],
+              'nb_epoch':[100, 500],
+              'optimizer':['adam', 'rmsprop']}
+grid_search = GridSearchCV(estimator=classifier,
+                           param_grid=parameters,
+                           scoring = 'accuracy',
+                           cv=10)
+grid_search=grid_search.fit(X_train, y_train)
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
 
